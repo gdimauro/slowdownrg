55a0fbf50764929eb85ec5e0a2d7d63e3c5b9a15
Giuseppe Dimauro <gdimauro@codearchitects.com>
2020-05-10 21:45:24 +0200

    added title with counters

--- slowdownrg/Program.cs
+++ slowdownrg/Program.cs
@@ -13,26 +13,58 @@ namespace slowdownrg
 
       for (; ; )
       {
+        int idle = 0;
+        int belowNormal = 0;
+        int normal = 0;
+        int aboveNormal = 0;
+        int realTime = 0;
+        int high = 0;
         var all = Process.GetProcessesByName("rg").OrderBy(i => i.StartTime);
 
         int i = 10;
         int changed = 0;
-        foreach(var p in all)
+        foreach (var p in all)
         {
           try
           {
             var priorityClass = i >= 0 ? ProcessPriorityClass.BelowNormal : ProcessPriorityClass.Idle;
-            if (!p.HasExited && p.PriorityClass != priorityClass)
+            //var affinity = i >= 0 ? (IntPtr)7 : (IntPtr)1;
+            if (!p.HasExited && (p.PriorityClass != priorityClass /*|| p.ProcessorAffinity != affinity*/))
             {
               p.PriorityClass = priorityClass;
-              p.ProcessorAffinity = (IntPtr)0x7;
+              //p.ProcessorAffinity = affinity;
               changed++;
               Console.Write(i >= 0 ? '+' : '-');
             }
+            switch (priorityClass)
+            {
+              case ProcessPriorityClass.Idle:
+                idle++;
+                break;
+              case ProcessPriorityClass.BelowNormal:
+                belowNormal++;
+                break;
+              case ProcessPriorityClass.Normal:
+                normal++;
+                break;
+              case ProcessPriorityClass.AboveNormal:
+                aboveNormal++;
+                break;
+              case ProcessPriorityClass.RealTime:
+                realTime++;
+                break;
+              case ProcessPriorityClass.High:
+                high++;
+                break;
+            }
           }
-          catch { }
+          catch
+          {
+            Console.Write('*');
+          }
           i--;
         }
+        Console.Title = $"Total: {all.Count()} Delta: {changed} Idle: {idle} Below: {belowNormal} Normal: {normal} Above: {aboveNormal} Realtime: {realTime} High: {high}";
         System.Threading.Thread.Sleep(changed == 0 ? 500 : 100);
         //Console.Write("....");
       }
